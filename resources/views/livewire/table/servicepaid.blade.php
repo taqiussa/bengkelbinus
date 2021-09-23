747cd2fbefe263b2e54f10aea485b0359108857b
taqiussa <taqiussa@gmail.com>
2021-09-23 07:24:55 +0700

    service paid view

--- resources/views/livewire/table/servicepaid.blade.php
+++ resources/views/livewire/table/servicepaid.blade.php
@@ -7,14 +7,10 @@
                     @include('components.sort-icon', ['field' => 'id'])
                 </a></th>
                 <th><a role="button" href="#">
-                    Nama
-                </a></th>
-                <th><a role="button" href="#">
                     No Polisi
                 </a></th>
-                <th><a wire:click.prevent="sortBy('status')" role="button" href="#">
-                    Status
-                    @include('components.sort-icon', ['field' => 'status'])
+                <th><a role="button" href="#">
+                    Nama
                 </a></th>
                 <th><a wire:click.prevent="sortBy('total')" role="button" href="#">
                     Total
@@ -24,6 +20,10 @@
                     Tanggal Dibuat
                     @include('components.sort-icon', ['field' => 'created_at'])
                 </a></th>
+                <th><a wire:click.prevent="sortBy('status')" role="button" href="#">
+                    Status
+                    @include('components.sort-icon', ['field' => 'status'])
+                </a></th>
                 <th>Action</th>
             </tr>
         </x-slot>
@@ -31,11 +31,11 @@
             @foreach ($services as $key => $service)
                 <tr x-data="window.__controller.dataTableController({{ $service->id }})">
                     <td>{{ $services->firstItem() + $key }}</td>
-                    <td>{{ $service->customer->nama }}</td>
                     <td>{{ $service->customer->nopol }}</td>
-                    <td><a role="button" wire:click="edit({{ $service->id }})" class="mr-3 btn btn-primary">{{ $service->status }}</a></td>
+                    <td>{{ $service->customer->nama }}</td>
                     <td>Rp. {{ number_format($service->total,0,",",".") }}</td>
                     <td>{{ $service->created_at->format('d M Y H:i') }}</td>
+                    <td><a role="button" wire:click="edit({{ $service->id }})" class="mr-3 btn btn-primary">{{ $service->status }}</a></td>
                     <td class="whitespace-no-wrap row-action--icon">
                         <a role="button" wire:click="editpayment({{ $service->id }})" class="mr-3"><i class="fa fa-16px fa-print"></i></a>
                         <a role="button" x-on:click.prevent="deleteItem" href="#"><i class="text-red-500 fa fa-16px fa-trash"></i></a>
